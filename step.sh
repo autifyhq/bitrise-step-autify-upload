69de477e0418d0fb2e53d8966030972f96267954
tarappo <tarappo@gmail.com>
2021-05-24 07:00:58 +0900

    add bitrise step.sh for upload app

--- step.sh
+++ step.sh
@@ -3,7 +3,7 @@
 # Upload to Autify
 set -eux
 
-readonly STEP_VERSION=0.0.0
+readonly API_BASE_ADDRESS="https://mobile-app.autify.com/api/v1"
 
 info() {
   printf "$(tput setaf 7)- %s$(tput sgr0)\n" "$@"
@@ -18,7 +18,21 @@ error() {
 }
 
 main() {
-  #
+  TOKEN_HEADER="Authorization: Bearer ${UPLOAD_TOKEN}"
+  API_UPLOAD_ADDRESS="${API_BASE_ADDRESS}/projects/${PROJECT_ID}/builds"
+  RESPONSE=$(curl -X POST "${API_UPLOAD_ADDRESS}" -H "accept: application/json" -H "${TOKEN_HEADER}" -H "Content-Type: multipart/form-data" -F "file=@${APP_DIR_PATH};type=application/zip" -w '\n%{http_code}' -s)
+
+  # http status
+  HTTP_STATUS=$(echo "$RESPONSE" | tail -n 1)
+  # body
+  BODY=$(echo "$RESPONSE" | sed '$d')
+
+  if [[ "$HTTP_STATUS" != "201" ]]; then
+    error "$BODY"
+    exit 1
+  fi
+
+  success "$BODY"
 }
 
 main "$@"
