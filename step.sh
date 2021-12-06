bfe8269d5936fa876c12bb5777e5a921b002248d
tarappo <tarappo@gmail.com>
2021-12-06 11:39:26 +0900

    set download url for cli

--- step.sh
+++ step.sh
@@ -3,17 +3,19 @@
 # Upload to Autify
 set -e
 
+
 export AUTIFY_UPLOAD_TOKEN="$upload_token"
 export AUTIFY_PROJECT_ID="$project_id"
 export AUTIFY_APP_DIR_PATH="$app_dir_path"
+export AUTIFY_MOBILE_SCRIPT="https://raw.githubusercontent.com/autifyhq/autify-for-mobile-cli/main/autify_mobile_cli.sh"
+export AUTIFY_MOBILE_SCRIPT_NAME="autify_mobile_cli.sh"
 
 readonly API_BASE_ADDRESS="https://mobile-app.autify.com/api/v1"
 readonly WORKIND_DIR="./"
 readonly ZIP_NAME="upload.zip"
 
+# download script
+curl -L -o "./$AUTIFY_MOBILE_SCRIPT_NAME" $AUTIFY_MOBILE_SCRIPT
+chmod 777 "./$AUTIFY_MOBILE_SCRIPT_NAME"
 
-# TODO: download script
-
-
-SCRIPT_DIR=$(unset CDPATH && cd "${0%/*}" &>/dev/null && pwd)
-exec "$SCRIPT_DIR"/autify_mobile_cli.sh
+exec "./$AUTIFY_MOBILE_SCRIPT_NAME"
