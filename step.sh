c1a34f4501e82c4b4b04f8d4f6c25a40b0f65380
tarappo <tarappo@gmail.com>
2022-04-18 05:35:46 +0900

    set disable=SC2154

--- step.sh
+++ step.sh
@@ -3,10 +3,13 @@
 # Upload to Autify
 set -e
 
-
+# shellcheck disable=SC2154
 export AUTIFY_UPLOAD_TOKEN="$upload_token"
+# shellcheck disable=SC2154
 export AUTIFY_PROJECT_ID="$project_id"
+# shellcheck disable=SC2154
 export AUTIFY_APP_DIR_PATH="$app_dir_path"
+
 export AUTIFY_MOBILE_SCRIPT="https://raw.githubusercontent.com/autifyhq/autify-for-mobile-cli/main/autify_mobile_cli.sh"
 export AUTIFY_MOBILE_SCRIPT_NAME="autify_mobile_cli.sh"
 
